b82001de88efd9d4d883d4eda6eec8b766a24ca9
Agustina Fornés <101611230+FornesAgustina@users.noreply.github.com>
2022-11-21 21:51:15 +0000

    update final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   
-from flask import Flask, render_template, request, json, jsonify, redirect
-import random
+from flask import Flask, render_template, request, json, jsonify, redirect, session
+from flask_session import Session
 import sqlite3, os
 from os.path import abspath, dirname, join
 from werkzeug.utils import secure_filename
@@ -12,7 +12,10 @@ path2 = 'img'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = 'contraseña2000'
+Session(app)
 
 #IMAGENES EN HTML y Traer DATOS
 #usuario
@@ -45,8 +48,13 @@ def registroAjax():
       else: 
         q = f"""INSERT INTO Usuario (nombre, mail, contraseña, tipoUsuario)  VALUES ('{nombre}', '{mail}', '{contraseña}', '{tipo}')"""
         print(q)
-        agregar = conn.execute(q)
-        print(agregar)
+        conn.execute(q)
+
+        a = f"""SELECT id_usuario FROM Usuario WHERE nombre = '{nombre}'"""
+        resu = conn.execute(a)
+        existe = resu.fetchone()
+        print(existe[0])
+        session["id"] = existe[0]
         conn.commit()
         conn.close()
         return redirect ("/mapa") 
@@ -66,22 +74,23 @@ def ingresoAjax():
       nombre = request.form['nombre']
       contraseña = request.form['contraseña']
     
-      
       print(nombre)
       print(contraseña)
 
-    
-
       conn = sqlite3.connect('BD_RDS.db')
     
-      q = f"""SELECT nombre, contraseña, tipoUsuario FROM Usuario WHERE nombre = '{nombre}' and contraseña = '{contraseña}' """
+      q = f"""SELECT id_usuario, nombre, contraseña, tipoUsuario FROM Usuario WHERE nombre = '{nombre}' and contraseña = '{contraseña}' """
       print(q)
       resu = conn.execute(q)
       account = resu.fetchone()
       
-      if account and account[2] == 'admin':
+      if account and account[3] == 'admin':
+        print(account[0])
+        session["id"] = account[0]
         return redirect ("/admin")  
-      elif account and account[2] == 'cliente':
+      elif account and account[3] == 'cliente':
+        print(account[0])
+        session["id"] = account[0]
         return redirect ("/mapa")
       else:
         print("No existe, registrese")
@@ -99,9 +108,15 @@ def administrador():
   q = """SELECT id_lugar, nombre FROM Lugar """
   respuesta = conn.execute(q)
   print(respuesta)
+  
+  i = """SELECT id_imagen, url_img FROM Imagenes """
+  img = conn.execute(i)
+  print(img)
+  
   lista = respuesta.fetchall()
+  list = img.fetchall()
   print(lista)
-  return render_template('admin.html', lista = lista)
+  return render_template('admin.html', lista = lista, list = list)
   
 
 @app.route('/eliminarAjax',  methods=['DELETE'])
@@ -110,7 +125,6 @@ def eliminarAjax():
       variable = request.form['lugar']
       
       conn = sqlite3.connect('BD_RDS.db')
-      
       q = f"""DELETE FROM Lugar WHERE nombre = '{variable}' """
       print(q)
       eliminar = conn.execute(q)
@@ -119,7 +133,33 @@ def eliminarAjax():
       print(eliminar)
       return json.dumps(True)
 
+@app.route('/eliminar',  methods=['DELETE'])
+def eliminar():
+    if request.method == 'DELETE':
+      variable = request.form['img']
+      
+      conn = sqlite3.connect('BD_RDS.db')
+      q = f"""DELETE FROM Imagenes WHERE url_img = '{variable}' """
+      print(q)
+      eliminar = conn.execute(q)
+      conn.commit()
+      conn.close()
+      print(eliminar)
+      return json.dumps(True)
 
+"""@app.route('/eliminarOPAjax',  methods=['DELETE'])
+def eliminarOpAjax():
+    if request.method == 'DELETE':
+      
+      conn = sqlite3.connect('BD_RDS.db')
+      q = DELETE FROM Opiniones
+      print(q)
+      eliminar = conn.execute(q)
+      conn.commit()
+      conn.close()
+      print(eliminar)
+      return json.dumps(True)
+"""      
 @app.route('/modificarAjax',  methods=['PUT'])
 def modificarAjax():
     if request.method == 'PUT':
@@ -275,47 +315,52 @@ def buscador():
     input = request.form["input"]
     conn = sqlite3.connect('BD_RDS.db')
     q = f"""SELECT * FROM Lugar WHERE nombre like '{input}%' """
+    a = """SELECT * FROM Imagenes """
+    imagenes = conn.execute(a)
     respuesta = conn.execute(q)
 
-    results = {'lista': respuesta.fetchall()}
+    results = {'lista': respuesta.fetchall(), 'imagenes': imagenes.fetchall()}
     print(results)
     return jsonify(results)
   else:
     return render_template('mapa.html')
 
-  # Fin buscador  
-# FIN MAPA
+  # Fin buscador 
 
-# COORDENADAS
-
-
-
-
-
-
-
-
+  # COORDENADAS
 @app.route('/traerCoordenadas', methods=['POST', 'GET'])
 def traerCoordenadas():
   if request.method == "POST":
     #id = request.form["laId"]
     
     conn = sqlite3.connect('BD_RDS.db')
-    q = """SELECT coord_LAT, coord_LNG FROM Lugar"""
+    q = """SELECT coord_LAT, coord_LNG, id_lugar, nombre FROM Lugar"""
     respuesta = conn.execute(q)
-
+    
     results = {'coordenadas': respuesta.fetchall()}
     print(results)
     return jsonify(results)
     
 # COORDENADAS
 
+
+
+
+
+    
+# FIN MAPA
+
+
+
 # DESCRIPCION
 @app.route('/descripcion', methods=['POST', 'GET'])
 def descripcion():
   if request.method == "POST":
+    #id del lugar
     id = request.form['lugar']
+    session["id_lugar"] = id
     print(id)
+    #se muestra info y foto
     conn = sqlite3.connect('BD_RDS.db')
     q = f"""SELECT * FROM Lugar WHERE id_lugar == '{id}'"""
     respuesta = conn.execute(q)
@@ -323,15 +368,51 @@ def descripcion():
     imagenes = conn.execute(q)
     vector =  respuesta.fetchall()
     img =  imagenes.fetchall()
-    print(vector)
-    print(img)
-    print(img)
-    print(vector[0][2])
+    print(session["id"])
+    print(session["id_lugar"])
     return render_template('descripcion.html', NOMBRE=vector[0][1], DIRECCION=vector[0][2], INSTA=vector[0][8], DESCRIPCION=vector[0][3], IMAGEN=img)
-
+   
   else:
-        return render_template('descripcion.html')
+    return render_template('descripcion.html')
+    
+@app.route('/opinionAjax',  methods=['PUT'])
+def opiniones():
+    if request.method == 'PUT':
+      opinion = request.form['opinion']
+      estrellas = request.form['estrellas']
+      
+      print(opinion)
+      
+      conn = sqlite3.connect('BD_RDS.db')
+      
+      q = f"""INSERT INTO Opiniones (opinion, estrellas, id_usuario, id_lugar) VALUES ('{opinion}', '{estrellas}', '{session["id"]}', '{session["id_lugar"]}');"""
+      conn.execute(q)
+
+      
+      a = f"""SELECT * FROM Opiniones WHERE id_lugar = '{session["id_lugar"]}'"""
+      op = conn.execute(a)
+      mostrar = op.fetchall()
+      conn.commit()
+      conn.close()
+      #print(modificar)
+      print(mostrar)
+      return render_template('descripcion.html')
+    else:
+      return render_template('descripcion.html')
+
+@app.route('/mostrarOpiniones', methods=['POST', 'GET'])
+def mostrarOpiniones():
+  if request.method == "GET":
+    conn = sqlite3.connect('BD_RDS.db')
+    q = f"""SELECT U.nombre, O.opinion From Opiniones AS O INNER JOIN Usuario AS U
+    ON U.id_usuario = O.id_usuario WHERE id_lugar = '{session["id_lugar"]}'"""
+    respuesta = conn.execute(q)
+    result = {'lista': respuesta.fetchall()}
+    print(result)
+    return jsonify(result)
 # FIN DESCRIPCION   
+
+ 
 app.run(host='0.0.0.0', port=81)
 
 
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -31,6 +31,57 @@ function eliminarAjax(){
     },
   });
 }
+
+/* Eliminar Imagenes */
+function eliminar(){
+  imagen = document.getElementById("eliminarImg").value
+  console.log(imagen)
+  $.ajax({
+    url:"/eliminar",
+    type:"DELETE",
+    data: {"img":imagen},
+    success: function(response){
+      console.log(response)
+      verificacion = JSON.parse(response);
+      opciones = document.getElementById("eliminarImg").options
+      html = ""
+      for (let i = 0; i < opciones.length; i++) {
+        /*console.log(opciones[i].value)*/
+        if (img != opciones[i].value) {
+          html += opciones[i].outerHTML
+          console.log(html);
+          console.log(i);
+        }
+      }
+      console.log(html);
+     
+      document.getElementById("eliminarImg").innerHTML=html
+     
+    },
+    
+    error: function(error){
+      console.log(error);
+    },
+  });
+}
+
+
+/* Eliminar Opiniones 
+function eliminarOp(){
+  $.ajax({
+    url:"/eliminarOPAjax",
+    type:"DELETE",
+    data: {"value":null},
+    success: function(response){
+      console.log(response)
+    },
+    
+    error: function(error){
+      console.log(error);
+    },
+  });
+}*/
+
 /* Modificar Lugar */
 function modificarAjax(){
   lugar = document.getElementById("lugar").value
@@ -82,9 +133,9 @@ function agregarAjax(){
       alert(`Nuevo lugar ingresado: ${verificacion}`)
       
         document.getElementById("lugar").innerHTML=document.getElementById("lugar").innerHTML+'<option value="'+nombre+'">' + nombre + '</option>';
-      console.log("Hola");
+      
 
-      document.getElementById("selectEliminar").innerHTML=document.getElementById("selectEliminar").innerHTML+'<option value="'+nombre+'">' + nombre + '</option>';
+document.getElementById("selectEliminar").innerHTML=document.getElementById("selectEliminar").innerHTML+'<option value="'+nombre+'">' + nombre + '</option>';
 
       document.getElementById("img").innerHTML=document.getElementById("img").innerHTML+'<option value="'+nombre+'">' + nombre + '</option>';
     },
@@ -113,18 +164,12 @@ function mandarAjax() {
  }); 
 }
 
-/* FIN ADMIN */
+                                      /* FIN ADMIN */
 
 
  
-
-//document.getElementById('NOMBRE').innerHTML = frase;
-
-
-/* https://dev.to/am20dipi/how-to-build-a-simple-search-bar-in-javascript-4onf#test */
-
-/* MAPA */
-/* buscador*/
+                                      /* MAPA */
+  /* buscador*/
 function buscar() {
   input =document.getElementById("SearchBar").value  
   console.log(input);
@@ -132,10 +177,68 @@ function buscar() {
     url:"/buscador",
     type:"POST",
     data: {"input":input},
+    
     success: function(result){
       lista = result;
-      console.log(result)
- 
+      console.log(result) 
+      if (input == ""){
+        underBar() }
+      else{
+        //div 1
+        nombre1 = result.lista[0][1]
+        dire1 = result.lista[0][2]
+        tipo1 = result.lista[0][7]
+        precio1 = result.lista[0][4]
+        for(t=0;t<result.imagenes.length;t++){
+          if (result.imagenes[t][2]==result.lista[0][0]){
+            foto1 = result.imagenes[t][1] 
+          }
+        }
+
+        document.getElementById('img1').src = './static/img/'+foto1;  
+        document.getElementById('nombre1').innerHTML = nombre1;
+        document.getElementById('input1').value = result.lista[0][0];
+        document.getElementById('direccion1').innerHTML = dire1;
+        document.getElementById('tipo1').innerHTML = tipo1;
+        document.getElementById('precio1').innerHTML = precio1;
+
+        //div 2
+        nombre2 = result.lista[1][1]
+        dire2 = result.lista[1][2]
+        tipo2 = result.lista[1][7]
+        precio2 = result.lista[1][4]
+        
+        for(a=0;a<result.imagenes.length;a++){
+          if (result.imagenes[a][2]==result.lista[1][0]){
+            foto2 = result.imagenes[a][1]
+          }
+        }
+
+        document.getElementById('img2').src = './static/img/'+ foto2; 
+        document.getElementById('nombre2').innerHTML = nombre2;
+        document.getElementById('input2').value = result.lista[1][0];
+        document.getElementById('direccion2').innerHTML = dire2;
+        document.getElementById('tipo2').innerHTML = tipo2;
+        document.getElementById('precio2').innerHTML = precio2;
+
+        //div 3
+        nombre3 = result.lista[2][1]
+        dire3 = result.lista[2][2]
+        tipo3 = result.lista[2][7]
+        precio3 = result.lista[2][4]
+        for(q=0;q<result.imagenes.length;q++){
+          if (result.imagenes[q][2]==result.lista[2][0]){
+            foto3 = result.imagenes[q][1]
+          }
+        }
+        console.log(nombre3)
+        document.getElementById('img3').src = './static/img/'+foto3; 
+        document.getElementById('nombre3').innerHTML = nombre3;
+        document.getElementById('input3').value = result.lista[2][0];
+        document.getElementById('direccion3').innerHTML = dire3;
+        document.getElementById('tipo3').innerHTML = tipo3;
+        document.getElementById('precio3').innerHTML = precio3;
+      }    
     },
     error: function(error){
       console.log(error);
@@ -145,7 +248,7 @@ function buscar() {
 
 
 
-    /* traer info buscador*/
+  /* traer info buscador*/
 function underBar() {
   $.ajax({
    type: "GET",
@@ -157,7 +260,7 @@ function underBar() {
       
       console.log(result)
       r = Math.floor(Math.random() * result.lista.length)
-      console.log(r)
+      //console.log(r)
       
       nombre1 = result.lista[r][1]
       dire1 = result.lista[r][2]
@@ -169,12 +272,11 @@ function underBar() {
         }
       }
     
-      console.log(foto1)
-      
-      console.log(dire1)
-      console.log(nombre1)
-      console.log(tipo1)
-      console.log(precio1)
+      //console.log(foto1)
+      //console.log(dire1)
+      //console.log(nombre1)
+      //console.log(tipo1)
+      //console.log(precio1)
       
       document.getElementById('img1').src = './static/img/'+foto1;  
       document.getElementById('nombre1').innerHTML = nombre1;
@@ -184,28 +286,29 @@ function underBar() {
       document.getElementById('precio1').innerHTML = precio1;
 
       
-      i = Math.floor(Math.random() * result.lista.length)
-      for (; i == r ;) {
-        i = Math.floor(Math.random() * result.lista.length); }
-      console.log(i)
 
-      
+      let i = Math.floor(Math.random() * result.lista.length)
+      for (; i == r;) {
+        i = Math.floor(Math.random() * result.lista.length); }
+      //console.log(i)
       nombre2 = result.lista[i][1]
       dire2 = result.lista[i][2]
       tipo2 = result.lista[i][7]
       precio2 = result.lista[i][4]
+      
       for(a=0;a<result.imagenes.length;a++){
         if (result.imagenes[a][2]==result.lista[i][0]){
+          //console.log(result.imagenes[a][1])
           foto2 = result.imagenes[a][1]
         }
       }
    
-      console.log(dire2)
-      console.log(nombre2)
-      console.log(tipo2)
-      console.log(precio2)
+      //console.log(dire2)
+      //console.log(nombre2)
+      //console.log(tipo2)
+      //console.log(precio2)
       
-      document.getElementById('img2').src = './static/img/'+foto2; 
+      document.getElementById('img2').src = './static/img/'+ foto2; 
       document.getElementById('nombre2').innerHTML = nombre2;
       document.getElementById('input2').value = result.lista[i][0];
       document.getElementById('direccion2').innerHTML = dire2;
@@ -217,7 +320,7 @@ function underBar() {
       e = Math.floor(Math.random() * result.lista.length)
       for (; e == r || e == i ;) {
         e = Math.floor(Math.random() * result.lista.length); }
-      console.log(i)
+      //console.log(i)
 
       
       nombre3 = result.lista[e][1]
@@ -230,13 +333,14 @@ function underBar() {
         }
       }
    
-      console.log(dire3)
-      console.log(nombre3)
-      console.log(tipo3)
-      console.log(precio3)
+      //console.log(dire3)
+      //console.log(nombre3)
+      //console.log(tipo3)
+      //console.log(precio3)
       
       document.getElementById('img3').src = './static/img/'+foto3; 
       document.getElementById('nombre3').innerHTML = nombre3;
+      document.getElementById('input3').value = result.lista[e][0];
       document.getElementById('direccion3').innerHTML = dire3;
       document.getElementById('tipo3').innerHTML = tipo3;
       document.getElementById('precio3').innerHTML = precio3;
@@ -267,12 +371,36 @@ function traerImg() {
       function error(error) {
         console.log(error)
       }
- }); 
-  
+ });  
 }
+
+// coordenadas
+function traerCoordenadas() {
+   $.ajax({
+   type: "POST",
+   url: "/traerCoordenadas",
+
+   success:  
+    function(result) {
+      console.log(result)
+      for(let i = 0; i < result.coordenadas.length; i++) {
+        map.addMarker({lat: result.coordenadas[i][0], lng: result.coordenadas[i][1]},true, false, false, true, true, {},'<b>'+ result.coordenadas[i][3]+'</b> <form id="formu" action="/descripcion" method="POST"><input type="hidden" name="lugar" value="'+result.coordenadas[i][2]+'"><button type="submit" name="submit_param" value="submit_value" class="link-button">ver más</button></form>');
+
+      }
+    }, 
+    error:
+      function error(error) {
+        console.log(error)
+      }
+ }); 
+}
+// fin coordenadas
 /* FIN MAPA */
 
+
+
 /* DESCRIPCION */
+//trae info a descripcion
 function descripcionAjax() {
    $.ajax({
    type: "POST",
@@ -290,28 +418,19 @@ function descripcionAjax() {
       }
  }); 
 }
-
-/* FIN DESC */
-
-
-// coordenadas
-
-
-//LAT = result.coordenadas[y][0]
-//LNG = result.coordenadas[y][1]
-
-function traerCoordenadas() {
+//carga opiniones
+function opinionAjax() {
+  opinion = document.getElementById("opinion").value;
+  estrellas = document.getElementById("estrellas").value;
    $.ajax({
-   type: "POST",
-   url: "/traerCoordenadas",
-   //data: {"laId":id},
+   type: "PUT",
+   url: "/opinionAjax",
+   data: {"opinion":opinion, "estrellas":estrellas},
 
-   success:  
-    function(result) {
+   success: 
+    function(result) { 
       console.log(result)
-      for(let i = 0; i < result.coordenadas.length; i++) {
-        map.addMarker({lat: result.coordenadas[i][0], lng: result.coordenadas[i][1]},true, false, true, true, true);
-      }
+      
     }, 
     error:
       function error(error) {
@@ -319,8 +438,97 @@ function traerCoordenadas() {
       }
  }); 
 }
-// fin coordenadas
 
-//`<p>${variable}</p>`
 
-// pisar sitios, hacer un for para los divs que necesitamos, `` sirven para pegar html
+//muestra opiniones
+function mostrarOp() {
+   $.ajax({
+   type: "GET",
+   url: "/mostrarOpiniones",
+   data: {},
+
+   success: 
+    function(result) {
+      for (let i =0; i < result.lista.length; i++){
+        usuario = result.lista[i][0]
+        opinion = result.lista[i][1]
+        document.getElementById("popup").innerHTML =  document.getElementById("popup").innerHTML + '<h5 value="'+result.lista[i]+'" name="usuario" id="usuario'+i+'">'+usuario+': </h5>';
+
+        //console.log('"usuario'+i+'"');
+        
+        document.getElementById("popup").innerHTML = document.getElementById("popup").innerHTML + '<p value="'+result.lista[i]+'"  name="opinion" id="opinion'+i+'">'+opinion+'</p>';
+      }  
+    },     
+  error:
+    function error(error) {
+      console.log(error)
+    }  
+  });
+}
+
+var btnAbrirPopup = document.getElementById('abrir'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+  btnAbrirPopup.addEventListener('click', function(){
+	  overlay.classList.add('active');
+  	popup.classList.add('active');
+  });     
+function cerraroPopUp(params) {
+  
+  $.ajax({
+   type: "GET",
+   url: "/mostrarOpiniones",
+   data: {},
+
+   success: 
+    function(result) {
+      for (let i =0; i < result.lista.length; i++){
+        usuario = result.lista[i][0]
+        opinion = result.lista[i][1]
+        
+        //const userEliminado = document.getElementsByName("usuario");
+        var ele= document.getElementsByName("usuario");
+        for(let i=0;i<ele.length;i++)
+        {
+          ele[i].parentNode.removeChild(ele[i]);
+        }
+        var opi= document.getElementsByName("opinion");
+        for(let i=0;i<opi.length;i++)
+        {
+          opi[i].parentNode.removeChild(opi[i]);
+        }
+
+        //console.log(userEliminado)
+        //='<h5 value="'+result.lista[i]+'" id="usuario'+i+'">'+usuario+'</h5>';
+
+        //const opinionEliminado = document.getElementsByName("opinion");//'<p value="'+result.lista[i]+'" id="opinion'+i+'">'+opinion+'</p>';
+        //userEliminado.remove();
+        //opinionEliminado.remove();
+      }  
+    },     
+  error:
+    function error(error) {
+      console.log(error)
+    }  
+  });
+
+  popup = document.getElementById('popup')
+  overlay = document.getElementById('overlay')
+  overlay.classList.remove('active');
+  popup.classList.remove('active');
+}
+/* FIN DESC 
+
+ <h5 value="" id="usuario">{{ lista[i][0] }}</h5>
+      <p value="" id="opinion">{{ lista[i][1] }}</p>*/
+
+
+/*valueUser = document.getElementById("usuario{{i}}").value 
+        valueOpinion =  document.getElementById("usuario{{i}}").value
+        if(valueUser = usuario && valueOpinion=opinion){
+          
+        }*/
+        //console.log(opinion)  <p value="'+opinion+'id="opinion'+i+'">'+opinion+'</p>'
+      //console.log(result.lista)
